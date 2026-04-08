c2afc40b3ee8566a4e5d6e32b5b1ac51a8932c8a
Darunhein <darayava2006@gmail.com>
2026-04-07 18:59:35 -0600

    formating

--- src/main/java/com/example/atm/ControllerMenu.java
+++ src/main/java/com/example/atm/ControllerMenu.java
@@ -1,11 +1,14 @@
 package com.example.atm;
 
 import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+
 import javafx.stage.Stage;
 
 import java.io.IOException;
