b8cb967335de08007c7dae733a99d5557b86503a
godeckc <20253rd015@utez.edu.mx>
2026-04-07 20:44:06 -0600

    soy Jesso

--- src/main/java/com/example/atm/ControllerConsult.java
+++ src/main/java/com/example/atm/ControllerConsult.java
@@ -1,4 +1,20 @@
 package com.example.atm;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class ControllerConsult {
+
+    private double saldo = 1000.0;
+    @FXML
+    private Label lblSaldo;
+    @FXML
+    void consultarSaldo(ActionEvent event) {
+        lblSaldo.setText("Saldo actual: $" +saldo);
+    }
+
+
+
+
 }
